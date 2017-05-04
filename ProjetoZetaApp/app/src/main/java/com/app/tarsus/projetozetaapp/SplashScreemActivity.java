21e36a48b5cef9021f5baa8b93c3d8103c9a17c1
Leandro Soares <leandro.moreira@dbmaster.com.br>
2017-05-04 12:39:25 -0300

    splash screem e Tela de login

---
+++ ProjetoZetaApp/app/src/main/java/com/app/tarsus/projetozetaapp/SplashScreemActivity.java
@@ -0,0 +1,13 @@
+package com.app.tarsus.projetozetaapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screem);
+    }
+}
